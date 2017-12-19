eed786c1e4cb38ff8bf5fb4474b5bebfcdada1a3
Arina Safiullina <31733496+arinasaf11@users.noreply.github.com>
2017-12-19 12:51:23 +0300

    Create func.py

---
+++ func.py
@@ -0,0 +1,19 @@
+def fact(n):
+    res = None
+    if type(n) is not int:
+            raise TypeError
+    try:                
+        n = int(n)
+    except (ValueError, TypeError) as e:
+        print('Argument "{}" is not a number '.format(n))
+    else: 
+        if n < 0:            
+            raise ValueError
+        elif n == 0:
+            return 1
+        else:
+            res = n*fact(n-1)
+            return res        
+
+if __name__ == '__main__':
+    unittest.main()
